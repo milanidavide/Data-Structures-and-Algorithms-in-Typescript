edc2edd434b280391153241cc8f42ac8b62e1d4a
Davide Milani <milanidavide@milanidavide.com>
2022-04-28 08:18:02 +0200

    feat: add quick find algorithm

---
+++ quick-find.ts
@@ -0,0 +1,42 @@
+/**
+ * Data Structure:
+ * - Integer array id[] of size n.
+ * - Interpretation: p and q are connected iff they have the same id.
+ *
+ * Time complexity:
+ * - initialize: O(n)
+ * - union: O(n)
+ * - find: O(1)
+ */
+
+class QuickFind {
+  private id: number[];
+
+  constructor(n: number) {
+    this.id = [];
+
+    for (let i = 0; i < n; i++) {
+      this.id[i] = i;
+    }
+  }
+
+  union(p: number, q: number): void {
+    const pid = this.id[p];
+    const qid = this.id[q];
+
+    for (let i = 0; i < this.id.length; i++) {
+      if (this.id[i] == pid) {
+        this.id[i] = qid;
+      }
+    }
+  }
+
+  find(p: number, q: number): boolean {
+    return this.id[p] === this.id[q];
+  }
+}
+
+const instance = new QuickFind(10);
+instance.union(0, 6);
+console.log(instance.find(0, 5)); // false;
+console.log(instance.find(0, 6)); // true;
